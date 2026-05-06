4b9577457ac548715eab95b4503e804831ee67e3
Eduardo1534 <pereiramartinezeduardo@gmail.com>
2026-05-06 19:45:10 -0300

    link para download

--- Template-Responsaveis/js/warnings.js
+++ Template-Responsaveis/js/warnings.js
@@ -1,6 +1,6 @@
 const warnings = [
     "⚠️⚠️ Para uso desta ferramenta se torna essencial o download do repositório !!! ⚠️⚠️",
-    "Link do Repositório https://github.com/Eduardo1534/Template-Organizacional-Tg.git",
+    "Link para Download https://github.com/Eduardo1534/Template-Organizacional-Tg/releases/tag/v0.8",
     "⚠️⚠️ Os nomes específicos de cada foto estão escritos em suas respectivas posições !!! Caso não lembre a ordem numérica exclua as fotos da pasta '/Monitores' e ou verifique o arquivo 'info.txt' ⚠️⚠️",
     "⚠️⚠️ As extensões de imagem aceitas são: jpg, jpeg, png e gif⚠️⚠️"
 ]
