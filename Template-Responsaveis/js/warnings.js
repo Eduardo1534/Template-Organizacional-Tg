f6b219046b1edaa195883b45f92cfa0015742333
Eduardo1534 <pereiramartinezeduardo@gmail.com>
2026-05-06 19:48:12 -0300

    Avisos

--- Template-Responsaveis/js/warnings.js
+++ Template-Responsaveis/js/warnings.js
@@ -1,4 +1,5 @@
 const warnings = [
+    "⚠️⚠️As fotos devem estar em ordem na pasta './moniores'⚠️⚠️",
     "⚠️⚠️ Para uso desta ferramenta se torna essencial o download do repositório !!! ⚠️⚠️",
     "Link para Download https://github.com/Eduardo1534/Template-Organizacional-Tg/releases/tag/v0.8",
     "⚠️⚠️ Os nomes específicos de cada foto estão escritos em suas respectivas posições !!! Caso não lembre a ordem numérica exclua as fotos da pasta '/Monitores' e ou verifique o arquivo 'info.txt' ⚠️⚠️",
